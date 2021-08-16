d047ef1457d6ae2b34ae25aca41079d7554317a5
ganeshchand@gmail.com <ganeshchand@gmail.com>
2021-08-16 22:38:20 +0000

    test commit

--- temperatures.py
+++ temperatures.py
@@ -52,3 +52,8 @@ display(df.select(roundedTemp("unit", "temp")))
 # COMMAND ----------
 
 display(df.select(convertFtoC("unit", "temp")))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Commit from Databricks workspace
